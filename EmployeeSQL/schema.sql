929d07bc4ecf377a6392ae46b0d906bb2d769956
ecosterlund <erikosterlund@comcast.net>
2021-03-04 22:18:37 -0500

    Working schema with csv imported. Need to work on queries in analysis

--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -1,16 +1,14 @@
 --------Creating Tables--------
--- CSV Header: emp_no,emp_title_id,birth_date,first_name,last_name,sex,hire_date
-
 --Create Titles
 CREATE TABLE titles (
-  title_id INT PRIMARY KEY,
+  title_id VARCHAR(30) PRIMARY KEY,
   title VARCHAR(30) NOT NULL
 );
 
 --Creating Employees Table
 CREATE TABLE employees (
   emp_no INT Primary Key,
-  emp_title_id INT NOT NULL,
+  emp_title_id VARCHAR(30) NOT NULL,
   birth_date DATE NOT NULL,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
@@ -22,33 +20,36 @@ CREATE TABLE employees (
 --Create Departments Table
 CREATE TABLE departments (
   dept_no VARCHAR(30) PRIMARY KEY,
-  dept_name VARCHAR(30) NOT NULL,
-  
+  dept_name VARCHAR(30) NOT NULL
 );
 
---Salaries Table
-CREATE TABLE salaries (
-  emp_no SERIAL PRIMARY KEY,
-  salary FLOAT NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
-);
-	
---Department Employee Table
+-- create a department employees table
 CREATE TABLE dept_emp (
-  emp_no SERIAL PRIMARY KEY,
-  dept_no VARCHAR(30) NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-  FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+        emp_no INT,
+	    dept_no VARCHAR(4),
+	    PRIMARY KEY (emp_no, dept_no),
+	    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	    FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
 
---Department Manager Table
+-- create a department managers table
 CREATE TABLE dept_manager (
-  dept_no VARCHAR(30) PRIMARY KEY,
-  emp_no INT NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-  FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+        dept_no VARCHAR(4),
+	    emp_no INT,
+	    PRIMARY KEY (emp_no, dept_no),
+	    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	    FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+-- create a department salaries table
+CREATE TABLE salaries (
+        emp_no INT PRIMARY KEY,
+	    salary INT,
+	    FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
+
+
+--Drop Tables
 Drop table dept_manager;
 Drop table dept_emp;
 Drop table salaries;
