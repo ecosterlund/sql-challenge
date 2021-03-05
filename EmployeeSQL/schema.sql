476c0167c42d8c2a89d48ba1603b26ebcc900ee5
ecosterlund <erikosterlund@comcast.net>
2021-03-04 19:48:28 -0500

    Setting up table structure and adding .sql file

---
+++ EmployeeSQL/schema.sql
@@ -0,0 +1,41 @@
+--CREATE TABLE departments (
+ -- dept_no SERIAL PRIMARY KEY,
+--  dept_name VARCHAR(10) NOT NULL,
+--  FOREIGN KEY (animal_id) REFERENCES animals_all(id)
+--);
+
+--CREATE TABLE dept_emp (
+ -- emp_no SERIAL PRIMARY KEY,
+--  dept_no VARCHAR() NOT NULL,
+--  FOREIGN KEY (animal_id) REFERENCES animals_all(id)
+--);
+
+--CREATE TABLE dept_manager (
+ -- dept_no SERIAL PRIMARY KEY,
+--  emp_no VARCHAR() NOT NULL,
+--  FOREIGN KEY (animal_id) REFERENCES animals_all(id)
+--);
+
+--emp_no,emp_title_id,birth_date,first_name,last_name,sex,hire_date
+--CREATE TABLE employees (
+ -- emp_no SERIAL PRIMARY KEY,
+--  emp_title_id VARCHAR() NOT NULL,
+--  birth_date VARCHAR() NOT NULL,
+--  first_name VARCHAR() NOT NULL,
+--  last_name VARCHAR() NOT NULL,
+--  sex VARCHAR() NOT NULL,
+--  hire_date VARCHAR() NOT NULL,
+--  FOREIGN KEY (animal_id) REFERENCES animals_all(id)
+--);
+
+--CREATE TABLE salaries (
+ -- emp_no SERIAL PRIMARY KEY,
+--  salary VARCHAR() NOT NULL,
+--  FOREIGN KEY (animal_id) REFERENCES animals_all(id)
+--);
+
+--CREATE TABLE titles (
+ -- title_id SERIAL PRIMARY KEY,
+--  title VARCHAR() NOT NULL,
+--  FOREIGN KEY (animal_id) REFERENCES animals_all(id)
+--);
