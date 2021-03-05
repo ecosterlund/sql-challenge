dae0b19b42efaf29f97551b5b962f0d962a2d638
ecosterlund <erikosterlund@comcast.net>
2021-03-04 21:57:49 -0500

    Tables created, issues with importing csv files. May have something to do with order

--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -1,41 +1,61 @@
---CREATE TABLE departments (
- -- dept_no SERIAL PRIMARY KEY,
---  dept_name VARCHAR(10) NOT NULL,
---  FOREIGN KEY (animal_id) REFERENCES animals_all(id)
---);
+--------Creating Tables--------
+-- CSV Header: emp_no,emp_title_id,birth_date,first_name,last_name,sex,hire_date
 
---CREATE TABLE dept_emp (
- -- emp_no SERIAL PRIMARY KEY,
---  dept_no VARCHAR() NOT NULL,
---  FOREIGN KEY (animal_id) REFERENCES animals_all(id)
---);
+--Create Titles
+CREATE TABLE titles (
+  title_id INT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL
+);
 
---CREATE TABLE dept_manager (
- -- dept_no SERIAL PRIMARY KEY,
---  emp_no VARCHAR() NOT NULL,
---  FOREIGN KEY (animal_id) REFERENCES animals_all(id)
---);
+--Creating Employees Table
+CREATE TABLE employees (
+  emp_no INT Primary Key,
+  emp_title_id INT NOT NULL,
+  birth_date DATE NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  sex VARCHAR(30) NOT NULL,
+  hire_date DATE NOT NULL,
+  FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+);
 
---emp_no,emp_title_id,birth_date,first_name,last_name,sex,hire_date
---CREATE TABLE employees (
- -- emp_no SERIAL PRIMARY KEY,
---  emp_title_id VARCHAR() NOT NULL,
---  birth_date VARCHAR() NOT NULL,
---  first_name VARCHAR() NOT NULL,
---  last_name VARCHAR() NOT NULL,
---  sex VARCHAR() NOT NULL,
---  hire_date VARCHAR() NOT NULL,
---  FOREIGN KEY (animal_id) REFERENCES animals_all(id)
---);
+--Create Departments Table
+CREATE TABLE departments (
+  dept_no VARCHAR(30) PRIMARY KEY,
+  dept_name VARCHAR(30) NOT NULL,
+  
+);
 
---CREATE TABLE salaries (
- -- emp_no SERIAL PRIMARY KEY,
---  salary VARCHAR() NOT NULL,
---  FOREIGN KEY (animal_id) REFERENCES animals_all(id)
---);
+--Salaries Table
+CREATE TABLE salaries (
+  emp_no SERIAL PRIMARY KEY,
+  salary FLOAT NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+	
+--Department Employee Table
+CREATE TABLE dept_emp (
+  emp_no SERIAL PRIMARY KEY,
+  dept_no VARCHAR(30) NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+  FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
 
---CREATE TABLE titles (
- -- title_id SERIAL PRIMARY KEY,
---  title VARCHAR() NOT NULL,
---  FOREIGN KEY (animal_id) REFERENCES animals_all(id)
---);
+--Department Manager Table
+CREATE TABLE dept_manager (
+  dept_no VARCHAR(30) PRIMARY KEY,
+  emp_no INT NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+  FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+Drop table dept_manager;
+Drop table dept_emp;
+Drop table salaries;
+Drop table departments;
+Drop table employees;
+Drop table titles;
+
+
+
+
